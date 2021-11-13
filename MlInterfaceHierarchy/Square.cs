28a2bd3e424a54b574e05b8766b7929358e9aab2
Tigris <tabula.tenebris@gmail.com>
2021-11-14 04:19:27 +0600

    Add project files.

---
+++ MlInterfaceHierarchy/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MlInterfaceHierarchy
+{
+    class Square : IShape
+    {
+        void IDrawable.Draw()
+        {
+            //Draw on screen
+        }
+
+        void IPrintable.Draw()
+        {
+            //Draw to printer
+        }
+
+        int IShape.GetNumberOfSides()
+        {
+            return 4;
+        }
+
+        void IPrintable.Print()
+        {
+            //Print
+        }
+    }
+}
